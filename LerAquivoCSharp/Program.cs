09e841a60fb6094d8dd6d0b75776053ecb86bb29
Thiago Corrêa <99252640+thiagocorreabotelho@users.noreply.github.com>
2022-12-03 09:52:17 -0300

    Finalização do projeto para ler arquivos

---
+++ LerAquivoCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿const string FILE_PATH = "C:\\Correa Devs\\Cursos\\CSharp\\LerArquivoCSharp\\LerArquivosCSharp\\LerAquivoCSharp\\text-file.txt";
+
+
+//var data = File.ReadAllText(FILE_PATH);
+//Console.WriteLine(data);
+
+//var data = File.ReadAllLines(FILE_PATH);
+//var count = 0;
+//foreach (var line in data)
+//{
+//    count++;
+//    Console.WriteLine($"Linha: {count} - {line}");
+//}
+
+using var file = new StreamReader(FILE_PATH);
+string? line;
+
+while ((line = file.ReadLine()) != null)
+    Console.WriteLine(line);
+
+file.Close();
